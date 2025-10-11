759087383f35ac336a98b273026d5e508a0a7052
your-username <rkuzsma@linksquares.com>
2025-10-10 20:54:50 -0400

    Fix specs

--- lib/schema_tools.rb
+++ lib/schema_tools.rb
@@ -3,4 +3,7 @@ require 'schema_tools/rake_tasks'
 # Load Railtie for automatic rake task loading in Rails apps
 if defined?(Rails)
   require 'schema_tools/railtie'
+else
+  # For non-Rails usage, load tasks immediately
+  SchemaTools::RakeTasks.load_tasks
 end
--- lib/schema_tools/rake_tasks.rb
+++ lib/schema_tools/rake_tasks.rb
@@ -4,12 +4,19 @@ require 'rake/tasklib'
 module SchemaTools
   module RakeTasks
   def self.load_tasks
-    # Only load schema.rake, not test.rake which requires development dependencies
+    # Load schema.rake
     schema_rake_file = File.join(File.dirname(__FILE__), '..', 'tasks', 'schema.rake')
     load schema_rake_file if File.exist?(schema_rake_file)
+    
+    # Load test.rake only in development/test environment
+    if defined?(Rails) && (Rails.env.development? || Rails.env.test?) || 
+       !defined?(Rails) && (ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test' || ENV['RAILS_ENV'].nil?)
+      test_rake_file = File.join(File.dirname(__FILE__), '..', 'tasks', 'test.rake')
+      load test_rake_file if File.exist?(test_rake_file)
+    end
   end
   end
 end
 
-# Auto-load tasks when this file is required
-SchemaTools::RakeTasks.load_tasks
+# Tasks are loaded automatically in Rails apps via Railtie
+# For non-Rails usage, call SchemaTools::RakeTasks.load_tasks manually

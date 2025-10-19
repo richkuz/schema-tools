7d62a61956859a77e4fd12a0084570b7222b5ec5
your-username <rkuzsma@linksquares.com>
2025-10-19 11:22:54 -0400

    Better example custom seeder

--- schemas/toys/doc_seeder.rb
+++ schemas/toys/doc_seeder.rb
@@ -1,10 +1,16 @@
+require 'securerandom'
+require 'time'
+
 # schema:seed invokes this class when seeding test data for this index
 class DocSeeder
   def initialize(index_or_alias_name) end
   def generate_document
     return {
       'name' => ['Basketball', 'Football', 'Baseball'].sample,
-      'description' => 'This is a cool toy!'
+      'description' => SchemaTools::Seeder::MappingsDocSeeder.generate_text_value,
+      'id' => SecureRandom.uuid,
+      'created_at' => (Time.now - (365 * 24 * 60 * 60)).iso8601,
+      'updated_at' => Time.now.iso8601
     }
   end
 end
